ac1b8b967dbf1e92e5e308ff5ce284998e4c7003
MartinITHS <martin@emsound.se>
2024-08-20 10:51:20 +0200

    Update

--- emsound/src/app/page.jsx
+++ emsound/src/app/page.jsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import Lenis from 'lenis'
 
 export default function Home() {
+
+  const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div> hi </div>
+      
     </main>
   );
 }
